ad8a48d311427c182ba1b2e96741eefd1abf6980
caseytirrell <tirrellcasey@gmail.com>
2024-05-13 11:29:41 -0400

    Initial Commit

---
+++ Fetch Project/Fetch_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ProjectApp.swift
+//  Fetch Project
+//
+//  Created by Casey tirrell on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
